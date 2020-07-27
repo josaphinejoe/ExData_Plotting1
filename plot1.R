087b6fb7f1474a0761f64a39788ec4cf2bcc32de
irene <35378135+josaphinejoe@users.noreply.github.com>
2020-07-27 23:04:09 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,8 +1,16 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-#str(subSetData)
-globalActivePower <- as.numeric(subSetData$Global_active_power)
+powerdata <- read.table("./household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";"  )
+powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
+powerdata$Time <- format(powerdata$Time, format="%H:%M:%S")
+powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)
+powerdata$Global_reactive_power <- as.numeric(powerdata$Global_reactive_power)
+powerdata$Voltage <- as.numeric(powerdata$Voltage)
+powerdata$Global_intensity <- as.numeric(powerdata$Global_intensity)
+powerdata$Sub_metering_1 <- as.numeric(powerdata$Sub_metering_1)
+powerdata$Sub_metering_2 <- as.numeric(powerdata$Sub_metering_2)
+powerdata$Sub_metering_3 <- as.numeric(powerdata$Sub_metering_3)
+subsetdata <- subset(powerdata, Date == "2007-02-01" | Date =="2007-02-02")
+        
+ ## plot histogram of global active power for those 2 days
 png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(subsetdata$Global_active_power, col="red", border="black", main ="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
 dev.off()
